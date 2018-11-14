0faa7b4f6eca7c653cd55f6a337538e281b9d800
Pavel.k <pavel.k@la-mark.com>
2018-11-14 16:31:20 +0200

    *Mobile menu - done

--- src/app/mobile-menu/mobile-menu.component.ts
+++ src/app/mobile-menu/mobile-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { useAnimation,trigger,state,style,animate,transition} from '@angular/animations';
+import { MenuTextItem } from '../menuTextItem'
 
 @Component({
   selector: 'app-mobile-menu',
@@ -19,10 +20,65 @@ export class MobileMenuComponent implements OnInit {
 
   isMenuOpen = false;
 
+  ourAppLoc: MenuTextItem = {
+    engText: 'our app',
+    prText: '',
+    spText: ''
+  };
+
+  learningDisneyLoc: MenuTextItem = {
+    engText: 'learing with disney',
+    prText: '',
+    spText: ''
+  };
+
+  kantooBlogLoc: MenuTextItem = {
+    engText: 'kantoo blog',
+    prText: '',
+    spText: ''
+  };
+
+  reviewsLoc: MenuTextItem = {
+    engText: 'reviews',
+    prText: '',
+    spText: ''
+  };
+
+  legacyLoc: MenuTextItem = {
+    engText: 'our legacy',
+    prText: '',
+    spText: ''
+  };
+
+  contactLoc: MenuTextItem = {
+    engText: 'contact',
+    prText: '',
+    spText: ''
+  };
+
+  language: MenuTextItem = {
+    engText: 'language',
+    prText: '',
+    spText: ''
+  }
+
+  menuAlpha=1;
+
   constructor() { }
 
   ngOnInit() {
+    console.log("mobileMenu")
+    window.addEventListener('scroll', this.scroll, true); //third parameter
   }
+
+  scroll = (): void => {
+    this.menuAlpha = 1 - window.pageYOffset / 250;
+    console.log(this.menuAlpha);
+      //handle your scroll here
+      //notice the 'odd' function assignment to a class field
+      //this is used to be able to remove the event listener
+
+    };
 
   toggleMenu(){
     this.isMenuOpen = !this.isMenuOpen;
